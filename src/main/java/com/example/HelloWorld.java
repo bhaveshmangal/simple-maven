7245d556e6560f24e23dfae8f239428d88a6c3c9
Khaled Nassar <knassar702@gmail.com>
2024-08-06 11:14:20 +0300

    things

---
+++ src/main/java/com/example/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.example;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        // Hardcoded sensitive information
+        String apiKey = "12345-ABCDE";
+        System.out.println("API Key: " + apiKey);
+        System.out.println("Hello, World!");
+    }
+}
