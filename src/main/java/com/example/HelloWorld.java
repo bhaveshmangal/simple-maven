3de79d6f6ff27c2daf5992a6a084a020d7a2b5a0
Khaled Nassar <45688522+knassar702@users.noreply.github.com>
2024-08-13 08:51:23 +0300

    Update HelloWorld.java

--- src/main/java/com/example/HelloWorld.java
+++ src/main/java/com/example/HelloWorld.java
@@ -3,7 +3,7 @@ package com.example;
 public class HelloWorld {
     public static void main(String[] args) {
         // Hardcoded sensitive information
-        String apiKey = "12345-ABCDE";
+        String apiKey = "12345-AssBCDE";
         System.out.println("API Key: " + apiKey);
         System.out.println("Hello, World!");
     }
